5d787e6f3b25a4637fd3746358af0601557ed1d0
Abdul <awsayed@alaska.edu>
2019-06-07 23:18:51 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    all << self
+  end
+
+  def self.all
+    @@all.map { |dog|
+      puts dog.name
+    }
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+end
